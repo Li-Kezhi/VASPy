4b221de1d90410143575bf821616571f9802cea3
Li-Kezhi <lkz13@mails.tsinghua.edu.cn>
2017-08-26 18:14:50 +0800

    Add script: transform XDATCAR to .xyz files

--- scripts/xdatcar_to_xyz.py
+++ scripts/xdatcar_to_xyz.py
@@ -50,10 +50,10 @@ with open(filename, 'r') as f:
                 content += '%16.9f%16.9f%16.9f\n' %\
                         (direct_coord[0], direct_coord[1], direct_coord[2])
 
-            out_name = 'xdatcar' + str(step) + '_test.xyz'
+            out_name = 'xdatcar' + str(step) + '.xyz'
             with open(out_name, 'w') as f1:
                 f1.write(content)
-            with open('xdatcar_test.xyz', 'a') as f2:
+            with open('xdatcar.xyz', 'a') as f2:
                 f2.write(content)
                 f2.close()
 
