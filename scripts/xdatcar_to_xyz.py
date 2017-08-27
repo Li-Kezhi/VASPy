0fa63f8b49df72d27935f930eca8a8923c016fae
Li-Kezhi <lkz13@mails.tsinghua.edu.cn>
2017-08-27 18:03:33 +0800

    Debug: consider compatibility for the forms of XDATCAR

--- scripts/xdatcar_to_xyz.py
+++ scripts/xdatcar_to_xyz.py
@@ -12,41 +12,55 @@ from vaspy.functions import str2list, line2list
 
 filename = 'XDATCAR'
 
-info_nline = 7
-with open(filename, 'r') as f:
+def __read_info(info):
+    '''
+    read lattice info
+    '''
     # read lattice info
-    system = f.readline().strip()
-    bases_const = float(f.readline().strip())
+    info['bases_const'] = float(f.readline().strip())
 
     # lattice basis
-    bases = []
+    info['bases'] = []
     for i in range(3):
-        basis = line2list(f.readline())
-        bases.append(basis)
+        vector = line2list(f.readline())
+        info['bases'].append(vector)
+
+
+info_nline = 7
+info = {}
+with open(filename, 'r') as f:
+    system = f.readline().strip()
+    __read_info(info)
 
     # atom info
-    atom_types = str2list(f.readline())
-    atoms_num = str2list(f.readline())
-    atom_numbers = [int(i) for i in atoms_num]
-    natom = sum(atom_numbers)
-    atom_names = []
-    for n, atom in zip(atoms_num, atom_types):
-        atom_names.extend([atom]*int(n))
+    info['atom_types'] = str2list(f.readline())
+    info['atoms_num'] = str2list(f.readline())
+    info['atom_numbers'] = [int(i) for i in info['atoms_num']]
+    info['natom'] = sum(info['atom_numbers'])
+    info['atom_names'] = []
+    for n, atom in zip(info['atoms_num'], info['atom_types']):
+        info['atom_names'].extend([atom]*int(n))
 
     while True:
         try:
             content = ''
-            for i in range(info_nline):
-                f.readline()  # skip info lines
+            flush = f.readline().strip()
+            if flush == system:
+                __read_info(info)  # update info
+                # skip atom info
+                f.readline()
+                f.readline()
+                step = f.readline().strip().split()[-1]
+            else:
+                step = flush.split()[-1]
 
-            step = f.readline().strip().split()[-1]
-            content += '          ' + str(natom) + '\n'
+            content += '          ' + str(info['natom']) + '\n'
             content += 'STEP =        ' + str(step) + '\n'
 
-            for i in range(natom):
-                content += '%2s' % atom_names[i]
+            for i in range(info['natom']):
+                content += '%2s' % info['atom_names'][i]
                 relative_coord = np.array(line2list(f.readline()), dtype='float64')
-                direct_coord = np.dot(bases, relative_coord) * bases_const
+                direct_coord = np.dot(info['bases'], relative_coord) * info['bases_const']
                 content += '%16.9f%16.9f%16.9f\n' %\
                         (direct_coord[0], direct_coord[1], direct_coord[2])
 
